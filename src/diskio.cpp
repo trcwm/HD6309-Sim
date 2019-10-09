7f972d125db9aeff35df54b3971efca708dd6b39
Niels Moseley <n.a.moseley@moseleyinstruments.com>
2019-10-10 01:38:59 +0200

    Overhauled drive emulation

--- src/diskio.cpp
+++ src/diskio.cpp
@@ -16,14 +16,48 @@
 
 //#define DEBUGPRINT
 
+#pragma pack(push, 1)
+struct SIR_t
+{
+  uint8_t volumeLabel[11];
+  uint8_t volumeNum[2];       // Big endian!!
+  uint8_t firstFreeTrack;
+  uint8_t firstFreeSector;
+  uint8_t lastFreeTrack;
+  uint8_t lastFreeSector;
+  uint8_t numFreeSectors[2];  // Big endian!!
+  uint8_t month;
+  uint8_t day;
+  uint8_t year;
+  uint8_t endTrack;
+  uint8_t endSector;
+};
+
+struct DIR_t
+{
+  uint8_t name[8];
+  uint8_t ext[3];
+  uint16_t dummy;
+  uint8_t startTrack;
+  uint8_t startSector;
+  uint8_t endTrack;
+  uint8_t endSector;
+  uint8_t totalSectorsHi;
+  uint8_t totalSectorsLo;
+  uint8_t flags;
+  uint8_t dummy2;
+  uint8_t month;
+  uint8_t day;
+  uint8_t year;
+};
+
+#pragma pack(pop)
+
+
 DiskIO::DiskIO()
 {
-    // 89.6K Shugart Shugart SA 400
-    m_tracks  = 35;
-    m_sectors = 10;
-    m_drives  = 4;
-
-    m_data.resize(256*m_tracks*m_sectors*m_drives);
+    // allocate the number of drives
+    m_drives.resize(4);
 
 #if 0
     setByte(0,0,5,16+0, 'D');
@@ -58,15 +92,55 @@ DiskIO::DiskIO()
     fclose(fout);
 
 #else
-    FILE *fin = fopen("flex9.dsk","rb");
+    loadImage(0, "flex9.dsk");
+    loadImage(1, "test.dsk");
+    loadImage(2, "test.dsk");
+    loadImage(3, "test.dsk");
+#endif
+}
+
+bool DiskIO::loadImage(uint8_t drive, const std::string &filename)
+{
+    if (drive >= m_drives.size())
+    { 
+        // drive does not exist!
+        return false;
+    }
+
+    FILE *fin = fopen(filename.c_str(),"rb");
     if (fin != 0)
     {
-        fread(&m_data[0], 1, 256*m_tracks*m_sectors, fin);
-        fclose(fin);
-        printf("Flex9 disk read!\n");
-    }    
-#endif
+        fseek(fin, 0, SEEK_END);
+        size_t bytes = ftell(fin);
+        rewind(fin);
 
+        if (bytes > 1024*1024*16)
+        {
+            // FLEX does not support more than 16MB!
+            return false;
+        }
+
+        m_drives[drive].resize(bytes);
+        fread(&m_drives[drive][0], 1, bytes, fin);
+        fclose(fin);
+        return true;
+    } 
+    return false;
+}
+
+bool DiskIO::getGeometry(uint8_t drive, uint8_t &tracks, uint8_t &sectors)
+{
+    if (drive < m_drives.size())
+    {
+        size_t ofs = 256*2 + 16;  // SIR record
+        SIR_t *sir = (SIR_t*)&m_drives[drive][ofs];
+
+        tracks  = sir->endTrack + 1;
+        sectors = sir->endSector;
+
+        return true;
+    }
+    return false;
 }
 
 void DiskIO::setByte(uint32_t drive, 
@@ -75,11 +149,18 @@ void DiskIO::setByte(uint32_t drive,
     uint32_t byteofs,
     uint8_t value)
 {
+    uint8_t tracks, sectors;
+    bool ok = getGeometry(drive, tracks, sectors);
+
+    if (!ok)
+    {
+        return;
+    }
+
     sector--;   // sectors start at 1
-    size_t ofs = 256*(sector + m_sectors*(track + m_tracks*drive));
+    size_t ofs = 256*(sector + sectors*track);
     
-    m_data[ofs+byteofs] = value;
-    //printf("        %d\n", (uint32_t)ofs);
+    m_drives[drive][ofs+byteofs] = value;
 }
 
 uint8_t DiskIO::readReg(uint8_t reg)
@@ -93,8 +174,18 @@ uint8_t DiskIO::readReg(uint8_t reg)
         if (m_cmd == READSECTOR)
         {            
             m_stat = 0;
-            size_t ofs = 256*((uint32_t)(m_sector-1) + m_sectors*((uint32_t)m_track + m_tracks*(uint32_t)m_drive));
-            uint8_t v = m_data[ofs+m_byteIdx];
+
+            uint8_t tracks, sectors;
+            bool ok = getGeometry(m_drive, tracks, sectors);
+
+            if (!ok) 
+            {
+                m_stat = 0xFF;
+                return 0;
+            }
+
+            size_t ofs = 256*((uint32_t)(m_sector-1) + sectors*(uint32_t)m_track);
+            uint8_t v = m_drives[m_drive][ofs+m_byteIdx];
 #ifdef DEBUGPRINT            
             printf("DISKIO: data[%u]: %d\n", (uint32_t)m_byteIdx, (uint32_t)v);
 #endif
@@ -145,8 +236,17 @@ void DiskIO::writeReg(uint8_t reg, uint8_t value)
 #endif        
         if (m_cmd == WRITESECTOR)
         {            
-            size_t ofs = 256*(m_sector + m_sectors*(m_track + m_tracks*m_drive));
-            m_data[ofs+m_byteIdx++] = value;
+            uint8_t tracks, sectors;
+            bool ok = getGeometry(m_drive, tracks, sectors);
+
+            if (!ok)
+            {
+                m_stat = 0xFF;
+                return;
+            }
+
+            size_t ofs = 256*((m_sector-1) + sectors*m_track);
+            m_drives[m_drive][ofs+m_byteIdx++] = value;
             m_stat = 0x00;
         }
         else
@@ -160,14 +260,6 @@ void DiskIO::writeReg(uint8_t reg, uint8_t value)
 #endif        
         m_drive = value;
         m_byteIdx = 0;
-        if (value < m_drives)
-        {
-            m_stat = 0;
-        }
-        else
-        {
-            m_stat = 0xFF;
-        }
         break;
     case 3: // track reg
 #ifdef DEBUGPRINT
@@ -175,14 +267,6 @@ void DiskIO::writeReg(uint8_t reg, uint8_t value)
 #endif            
         m_track = value;
         m_byteIdx = 0;
-        if (value < m_tracks)
-        {
-            m_stat = 0;
-        }
-        else
-        {
-            m_stat = 0xFF;
-        }        
         break;
     case 4: // sector reg
 #ifdef DEBUGPRINT
@@ -190,14 +274,6 @@ void DiskIO::writeReg(uint8_t reg, uint8_t value)
 #endif            
         m_sector = value;
         m_byteIdx = 0;
-        if (value < m_sectors)
-        {
-            m_stat = 0;
-        }
-        else
-        {
-            m_stat = 0xFF;
-        }        
         break;
     default:
 #ifdef DEBUGPRINT
--- src/diskio.h
+++ src/diskio.h
@@ -16,7 +16,7 @@
 
 #include <stdint.h>
 #include <vector>
-
+#include <string>
 
 /** fake disk I/O subsystem */
 class DiskIO
@@ -27,16 +27,16 @@ public:
     void writeReg(uint8_t reg, uint8_t value);
     uint8_t readReg(uint8_t reg);
 
+    bool loadImage(uint8_t drive, const std::string &filename);
+
 protected:
+    bool getGeometry(uint8_t drive, uint8_t &tracks, uint8_t &sectors);
+
     void setByte(uint32_t drive, 
         uint32_t track, 
         uint32_t sector, 
         uint32_t byteofs, 
         uint8_t value);
-
-    uint32_t m_sectors; ///< number of sector per track
-    uint32_t m_tracks;  ///< number of tracks per drive
-    uint32_t m_drives;  ///< number of drives
 
     uint8_t m_track;    ///< track register
     uint8_t m_sector;   ///< sector register
@@ -46,7 +46,7 @@ protected:
 
     uint8_t m_byteIdx;  ///< current byte index within sector
 
-    std::vector<uint8_t>    m_data; ///< the drive data
+    std::vector<std::vector<uint8_t> > m_drives;
 
     static constexpr uint8_t IDLECMD = 0;
     static constexpr uint8_t READSECTOR = 1;
